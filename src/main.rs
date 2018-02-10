3331a5bd28ec710e771d1ee4230ce06fde4befe0
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2018-02-10 05:34:28 -0200

    Static files, templates and a few more stuff to have the project basically up and running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket_contrib;
+extern crate rocket;
+
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+use rocket_contrib::Template;
+
+#[get("/")]
+fn index() -> Template {
+    let mut context = HashMap::new();
+
+    context.insert("foo", "bar-again");
+
+    Template::render("index", &context)
+}
+
+#[get("/static/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, files]).attach(
+        Template::fairing(),
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
